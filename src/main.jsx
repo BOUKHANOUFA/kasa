019459fb099b5632678dc722ffa2d3da3fb78fe7
boukhanoufa <arimasboukhanoufa@gmail.com>
2026-05-03 15:59:05 +0200

    réglage-main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import Header from './components/Header'
+import Footer from './components/Footer'
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
 import Logement from './pages/Logement.jsx'
-
 import Error from './pages/Error.jsx'
 
 createRoot(document.getElementById('root')).render(
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,3 +1,18 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+
 export default function Error() {
-  return <h1>Error</h1>;
+  return (
+    <>
+          <Header />
+         
+<div className="error">
+   <h1>Error</h1>
+   <h2>Oups! La page que vous demandez n'existe pas</h2>
+   <p>Retourner sur la page d'Accueil</p>
+  </div>
+   <Footer />
+   </>
+);
 }
