0e4ccf8a1518f2c1f943edbf7f9fe7a1588da016
boukhanoufa <arimasboukhanoufa@gmail.com>
2026-05-03 18:59:30 +0200

    Ajout de la page 404 et son css

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,8 @@
+import { Link } from "react-router-dom";
+import "./Error.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+
 
 
  function Error() {
@@ -8,9 +11,9 @@ import Footer from "../components/Footer";
           <Header />
          
 <div className="error">
-   <h1>404</h1>
-   <h2>Oups! La page que vous demandez n'existe pas</h2>
-   <p>Retourner sur la page d'Accueil</p>
+   <h1 className="error-number">404</h1>
+   <h2 className="error-text">Oups! La page que vous demandez n'existe pas</h2>
+   <Link to="/" className="error-link">Retourner sur la page d'Accueil</Link>
   </div>
    <Footer />
    </>
