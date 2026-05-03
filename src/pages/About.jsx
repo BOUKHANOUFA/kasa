78fdc459165ce01a60bd76af4b81dd143146b84e
boukhanoufa <arimasboukhanoufa@gmail.com>
2026-05-03 17:36:49 +0200

    Ajout de la page about.jsx+son css+ page error.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,19 @@
-export default function About() {
-  return <h1>About</h1>;
+import "./About.css";
+ import bannerImg from "../assets/about.svg";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+
+ function About() {
+  return (
+    <>
+          <Header />
+         
+<div className="about">
+   
+  </div>
+   <Footer />
+   </>
+);
 }
+export default About
