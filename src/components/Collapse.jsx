3f79d9ea8f61525ed23e91c828b83459b5994d80
boukhanoufa <arimasboukhanoufa@gmail.com>
2026-05-05 21:45:29 +0200

    css-collapse

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,18 +1,29 @@
+
+
+import "./Collapse.css";
 import { useState } from "react";
 
+function Collapse({ title, content }) {
+  const [open, setOpen] = useState(false);
 
+  return (
+    <div className="collapse">
 
-function Collapse({title ,content}) {
-  const [open, setOpen]=useState(false);
- return (
-   <div>
-      <button onClick={() => setOpen(!open)}>
-      {title}
-      </button>
- {open && <div>{content}</div>}
- </div>
- );
+      <div
+        className="collapse-button"
+        onClick={() => setOpen(!open)}
+      >
+        {title}
+      </div>
+
+      {open && (
+        <div className="collapse-content">
+          {content}
+        </div>
+      )}
+
+    </div>
+  );
 }
 
 export default Collapse;
-
