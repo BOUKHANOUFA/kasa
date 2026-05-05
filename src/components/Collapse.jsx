cfd15cccfc06c6a0556d28366308fa89bc95a073
boukhanoufa <arimasboukhanoufa@gmail.com>
2026-05-05 16:31:41 +0200

    création du composant Collapse avec ouverture/fermeture (useState)

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,5 +1,18 @@
-function Collapse() {
-  return <div>Test</div>;
+import { useState } from "react";
+
+
+
+function Collapse({title ,content}) {
+  const [open, setOpen]=useState(false);
+ return (
+   <div>
+      <button onClick={() => setOpen(!open)}>
+      {title}
+      </button>
+ {open && <div>{content}</div>}
+ </div>
+ );
 }
 
 export default Collapse;
+
