d018b1481c0ad91cd6be98c011d280e4a205d735
boukhanoufa <arimasboukhanoufa@gmail.com>
2026-05-06 21:25:42 +0200

    récupération id avec useParams + affichage dynamique

--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -1,8 +1,30 @@
+
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import "./Logement.css";
+import logements from "../data/logements";
+import { useParams } from "react-router-dom";
 
- function Logement() {
-  return <h1>Employees</h1>;
+function Logement() {
+  const { id } = useParams();
+
+  const logement = logements.find(item => item.id === id);
+
+  if (!logement) {
+    return <p>Logement introuvable</p>;
+  }
+
+  return (
+    <>
+      <Header />
+
+      <div>
+        <h1>{logement.title}</h1>
+        <img src={logement.cover} alt={logement.title} />
+      </div>
+
+      <Footer />
+    </>
+  );
 }
-export default Logement
+
+export default Logement;
